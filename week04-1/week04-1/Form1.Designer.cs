4a5eca9a481dc04f328101a8652f5cf6b6c8bb04
Tuğra GÜNDÜZ <trgndz1999@gmail.com>
2022-04-12 19:12:15 +0300

    Basic Picture Viewer
    Open&Save Image

---
+++ week04-1/week04-1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace week04_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnOpenImage = new System.Windows.Forms.Button();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.labelCoordinate = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(188, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // btnOpenImage
+            // 
+            this.btnOpenImage.Location = new System.Drawing.Point(12, 12);
+            this.btnOpenImage.Name = "btnOpenImage";
+            this.btnOpenImage.Size = new System.Drawing.Size(170, 30);
+            this.btnOpenImage.TabIndex = 1;
+            this.btnOpenImage.Text = "Open  Image";
+            this.btnOpenImage.UseVisualStyleBackColor = true;
+            this.btnOpenImage.Click += new System.EventHandler(this.btnOpenImage_Click);
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(12, 49);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(170, 30);
+            this.btnSaveFile.TabIndex = 2;
+            this.btnSaveFile.Text = "Save File";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // labelCoordinate
+            // 
+            this.labelCoordinate.AutoSize = true;
+            this.labelCoordinate.Location = new System.Drawing.Point(188, 269);
+            this.labelCoordinate.Name = "labelCoordinate";
+            this.labelCoordinate.Size = new System.Drawing.Size(37, 40);
+            this.labelCoordinate.TabIndex = 3;
+            this.labelCoordinate.Text = "X: 0\r\nY: 0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Close Form";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Exit Application";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 310);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelCoordinate);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnOpenImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnOpenImage;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.Label labelCoordinate;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ week04-1/week04-1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week04_1
+{
+    public partial class Form1 : Form
+    {
+        public bool isActive { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //e.Cancel = true;
+
+            DialogResult result = MessageBox.Show(
+                "Are you sure want to close the form?",
+                "Form closing...",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button2,
+                MessageBoxOptions.RightAlign
+                );
+
+            //if (result == DialogResult.Cancel)
+            //    e.Cancel = true;
+
+            e.Cancel = (result == DialogResult.Cancel);
+        }
+
+        private void btnOpenImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Title = "Select an Image";
+            ofd.Filter = "Gif Images|*.gif|JPEG Images|*.jpg";
+            ofd.FilterIndex = 2;
+            ofd.Multiselect = false;
+            DialogResult result = ofd.ShowDialog();
+
+            if (result == DialogResult.Cancel)
+                return;
+
+            Image image = Image.FromFile(ofd.FileName);
+            pictureBox1.Image = image;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+
+            //Graphics g = pictureBox1.CreateGraphics();
+            //g.DrawImage(image, new PointF(0, 0));
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.Title = "Select a File Location and Name";
+            sfd.Filter = "Gif Images|*.gif|JPEG Images|*.jpg";
+            sfd.FilterIndex = 2;
+            DialogResult result = sfd.ShowDialog();
+
+            if (result == DialogResult.Cancel)
+                return;
+
+
+            Image image = pictureBox1.Image;
+            image.Save(sfd.FileName);
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            labelCoordinate.Text = string.Format("X: {0}\nY: {1}", e.X, e.Y);
+
+            if (isActive)
+            {
+                Graphics g = pictureBox1.CreateGraphics();
+                g.DrawEllipse(Pens.DarkBlue, e.X, e.Y, 12, 12);
+            }            
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+
+            g.DrawRectangle(Pens.Maroon, e.X, e.Y, 32, 16);
+
+
+            isActive = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+            //this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isActive = true;
+        }
+    }
+}
