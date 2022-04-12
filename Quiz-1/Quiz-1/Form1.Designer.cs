8fee1e404bcf9e6d71a8e699db3adc3d1e62b92a
Tuğra GÜNDÜZ <trgndz1999@gmail.com>
2022-04-12 18:16:51 +0300

    Picture Viewer

---
+++ Quiz-1/Quiz-1/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿
+namespace Quiz_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnZoom = new System.Windows.Forms.Button();
+            this.btnCenter = new System.Windows.Forms.Button();
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.btnStretch = new System.Windows.Forms.Button();
+            this.btnNormal = new System.Windows.Forms.Button();
+            this.btnResetForm = new System.Windows.Forms.Button();
+            this.btnPicReset = new System.Windows.Forms.Button();
+            this.btnUIReset = new System.Windows.Forms.Button();
+            this.btnDrawBorder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(329, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "C:\\...";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(347, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 180);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(237, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnZoom);
+            this.groupBox1.Controls.Add(this.btnCenter);
+            this.groupBox1.Controls.Add(this.btnAuto);
+            this.groupBox1.Controls.Add(this.btnStretch);
+            this.groupBox1.Controls.Add(this.btnNormal);
+            this.groupBox1.Location = new System.Drawing.Point(336, 45);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(86, 180);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Size Mode";
+            // 
+            // btnZoom
+            // 
+            this.btnZoom.Location = new System.Drawing.Point(6, 135);
+            this.btnZoom.Name = "btnZoom";
+            this.btnZoom.Size = new System.Drawing.Size(75, 23);
+            this.btnZoom.TabIndex = 4;
+            this.btnZoom.Text = "Zoom";
+            this.btnZoom.UseVisualStyleBackColor = true;
+            this.btnZoom.Click += new System.EventHandler(this.btnZoom_Click);
+            // 
+            // btnCenter
+            // 
+            this.btnCenter.Location = new System.Drawing.Point(6, 106);
+            this.btnCenter.Name = "btnCenter";
+            this.btnCenter.Size = new System.Drawing.Size(75, 23);
+            this.btnCenter.TabIndex = 3;
+            this.btnCenter.Text = "Center";
+            this.btnCenter.UseVisualStyleBackColor = true;
+            this.btnCenter.Click += new System.EventHandler(this.btnCenter_Click);
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.Location = new System.Drawing.Point(6, 77);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(75, 23);
+            this.btnAuto.TabIndex = 2;
+            this.btnAuto.Text = "Auto";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.btnAuto_Click);
+            // 
+            // btnStretch
+            // 
+            this.btnStretch.Location = new System.Drawing.Point(6, 48);
+            this.btnStretch.Name = "btnStretch";
+            this.btnStretch.Size = new System.Drawing.Size(75, 23);
+            this.btnStretch.TabIndex = 1;
+            this.btnStretch.Text = "Stretch";
+            this.btnStretch.UseVisualStyleBackColor = true;
+            this.btnStretch.Click += new System.EventHandler(this.btnStretch_Click);
+            // 
+            // btnNormal
+            // 
+            this.btnNormal.Location = new System.Drawing.Point(6, 19);
+            this.btnNormal.Name = "btnNormal";
+            this.btnNormal.Size = new System.Drawing.Size(75, 23);
+            this.btnNormal.TabIndex = 0;
+            this.btnNormal.Text = "Normal";
+            this.btnNormal.UseVisualStyleBackColor = true;
+            this.btnNormal.Click += new System.EventHandler(this.btnNormal_Click);
+            // 
+            // btnResetForm
+            // 
+            this.btnResetForm.Location = new System.Drawing.Point(134, 276);
+            this.btnResetForm.Name = "btnResetForm";
+            this.btnResetForm.Size = new System.Drawing.Size(75, 23);
+            this.btnResetForm.TabIndex = 5;
+            this.btnResetForm.Text = "Reset Form";
+            this.btnResetForm.UseVisualStyleBackColor = true;
+            this.btnResetForm.Click += new System.EventHandler(this.btnResetForm_Click);
+            // 
+            // btnPicReset
+            // 
+            this.btnPicReset.Location = new System.Drawing.Point(12, 231);
+            this.btnPicReset.Name = "btnPicReset";
+            this.btnPicReset.Size = new System.Drawing.Size(75, 23);
+            this.btnPicReset.TabIndex = 6;
+            this.btnPicReset.Text = "Pic Preset";
+            this.btnPicReset.UseVisualStyleBackColor = true;
+            this.btnPicReset.Click += new System.EventHandler(this.btnPicReset_Click);
+            // 
+            // btnUIReset
+            // 
+            this.btnUIReset.Location = new System.Drawing.Point(134, 231);
+            this.btnUIReset.Name = "btnUIReset";
+            this.btnUIReset.Size = new System.Drawing.Size(75, 23);
+            this.btnUIReset.TabIndex = 7;
+            this.btnUIReset.Text = "UI Preset";
+            this.btnUIReset.UseVisualStyleBackColor = true;
+            this.btnUIReset.Click += new System.EventHandler(this.btnUIReset_Click);
+            // 
+            // btnDrawBorder
+            // 
+            this.btnDrawBorder.Location = new System.Drawing.Point(257, 231);
+            this.btnDrawBorder.Name = "btnDrawBorder";
+            this.btnDrawBorder.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawBorder.TabIndex = 8;
+            this.btnDrawBorder.Text = "Draw Border";
+            this.btnDrawBorder.UseVisualStyleBackColor = true;
+            this.btnDrawBorder.Click += new System.EventHandler(this.btnDrawBorder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 361);
+            this.Controls.Add(this.btnDrawBorder);
+            this.Controls.Add(this.btnUIReset);
+            this.Controls.Add(this.btnPicReset);
+            this.Controls.Add(this.btnResetForm);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Picture Viewer - 20170301006 - Tuğra GÜNDÜZ";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnZoom;
+        private System.Windows.Forms.Button btnCenter;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.Button btnStretch;
+        private System.Windows.Forms.Button btnNormal;
+        private System.Windows.Forms.Button btnResetForm;
+        private System.Windows.Forms.Button btnPicReset;
+        private System.Windows.Forms.Button btnUIReset;
+        private System.Windows.Forms.Button btnDrawBorder;
+    }
+}
+
---
+++ Quiz-1/Quiz-1/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.Lime;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            textBox1.BackColor = Color.White;
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Title = "Select an Image";
+            ofd.Filter = "Gif Images|*.gif|JPEG Images|*.jpg";
+            ofd.FilterIndex = 2;
+            ofd.Multiselect = false;
+            DialogResult result = ofd.ShowDialog();
+
+            if (result == DialogResult.Cancel)
+                return;
+
+            
+            Image image = Image.FromFile(ofd.FileName);
+            pictureBox1.Image = image;
+            textBox1.Text = ofd.FileName;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void btnResetForm_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "Are you sure want to reset the form?",
+                "Form is resetting...",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button2,
+                MessageBoxOptions.RightAlign
+                );
+
+            if (result == DialogResult.Yes)
+            {
+                textBox1.Text = @"C:\...";
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+                
+
+                Random random = new Random((int)DateTime.Now.Ticks);
+                int x = random.Next(0, 320 + 1);
+                int y = random.Next(0, 180 + 1);
+                Graphics g = pictureBox1.CreateGraphics();
+
+                if (x > y)
+                {
+                    g.DrawRectangle(Pens.Red, x, y, 80, 40);
+                }
+                else
+                {
+                    g.DrawRectangle(Pens.Red, y, x, 80, 40);
+                }
+                g.Clear(Color.White);
+            }
+
+        }
+
+        private void btnNormal_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+        }
+
+        private void btnStretch_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+        }
+
+        private void btnCenter_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+        }
+
+        private void btnZoom_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void btnPicReset_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.Empty);
+        }
+
+        private void btnUIReset_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            Random random = new Random();
+            int x = random.Next(0, 320 + 1);
+            int y = random.Next(0, 180 + 1);
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+            var bm = new Bitmap(pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            pictureBox1.DrawToBitmap(bm, pictureBox1.ClientRectangle);
+            g.DrawImage(bm, 0, 0);
+            g.DrawRectangle(Pens.Red, x, y, 80, 40);
+        }
+
+        private void btnDrawBorder_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            g.DrawRectangle(Pens.Red, 120, 70, 80, 40);
+        }
+    }
+}
